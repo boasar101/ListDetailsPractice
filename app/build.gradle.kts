c54669f94f6afb3846549d81c2741cb2f08c6f7d
bashar <hawarabashar20@gmail.com>
2025-12-27 02:49:04 +0500

    Add dependencies for local storage practice

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,7 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.constraintlayout.compose)
 
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
